94c39f07bd3c65ea9918108daea81ac5aa1b5ec3
Trace Durand <tdurand@tdurand-ltm.internal.salesforce.com>
2018-12-19 13:09:11 -0700

    All files added.

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = process.env.PORT || 8080
+var app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+	res.render('pages/index');
+}); 
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
